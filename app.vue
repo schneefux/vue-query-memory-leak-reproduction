f1c414ee057a5e1e764a1d81c3fa68f666eed4b6
schneefux <schneefux+commit@schneefux.xyz>
2023-02-21 15:27:46 +0100

    Init reproduction

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onServerPrefetch } from "vue";
+import { useQuery } from "@tanstack/vue-query";
+
+const fetcher = async () => 'memory-leak'
+
+const { suspense } = useQuery({ queryKey: ["test"], queryFn: fetcher });
+
+onServerPrefetch(suspense);
+</script>
